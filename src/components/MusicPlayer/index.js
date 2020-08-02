6dbc81aabb77f92058672bdcc18411550f52a1be
Dan <danielzuzevich@gmail.com>
2020-08-02 10:02:29 -0400

    all set

---
+++ src/components/MusicPlayer/index.js
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import React from 'react'
+import { css, jsx } from '@emotion/core'
+import Topbar from './Topbar'
+import Sidebar from './Sidebar'
+import Content from './Content'
+import Playbar from './Playbar'
+
+const MusicPlayer = () => (
+  <div css={CSS}>
+    <Topbar />
+    <Sidebar></Sidebar>
+    <Content></Content>
+    <Playbar></Playbar>
+  </div>
+)
+
+const CSS = css`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  position: relative;
+  color: white;
+`
+
+export default MusicPlayer
